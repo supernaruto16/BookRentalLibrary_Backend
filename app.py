5f2e9d717de3fbb26cbf48c4bde624ee589f7331
supernaruto16 <supernaruto16@gmail.com>
2019-12-18 13:42:20 +0700

    fix expired token

--- app.py
+++ app.py
@@ -52,6 +52,14 @@ def check_if_token_in_blacklist(decrypted_token):
     jti = decrypted_token['jti']
     return RevokedTokenModel.is_jti_blacklisted(jti)
 
+@jwt.expired_token_loader
+def my_expired_token_callback(expired_token):
+    token_type = expired_token['type']
+    return jsonify({
+        'status': 401,
+        'sub_status': 42,
+        'msg': 'The {} token has expired'.format(token_type)
+    }), 401
 
 api = Api(app, version='1.0', title='BookRental API',
           description='BookRental API')
